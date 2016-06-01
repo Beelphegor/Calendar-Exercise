13c3f8299d3b26ed0ae21c317157fabc3b959e7b
Juan Jose Guevara <juanjose@acklenavenue.com>
2016-05-31 20:57:36 -0600

    calendar generation working on multiple months

--- Calendar/src/app.js
+++ Calendar/src/app.js
@@ -16,49 +16,58 @@ app.controller("mainCtrl", function($scope) {
 
     function calculate() {
         var tempDate = new Date($scope.startDate.getTime());
-        //endDate.setDate($scope.startDate.getDate() + $scope.numberOfDays);
-        for (var i = 0; i <= $scope.numberOfDays; i++) {
+        var weekIndex = 0;
+        $scope.months = [];
+        for (var i = 0; i < $scope.numberOfDays; i++) {
             if (monthsAreEmpty()) {
                 $scope.months.push({
-                    month: tempDate.getMonth(),
+                    monthIndex: tempDate.getMonth(),
                     weeks: [{
-                        index: 0,
+                        index: weekIndex,
                         days: []
                     }]
                 });
-            } else {
-                if (tempDate.getDay() > 0 && $scope.months[$scope.months.length - 1].weeks[$scope.months[$scope.months.length - 1].weeks.length - 1].days.length === 0) {
-                    for (var z = 0; z < tempDate.getDay(); z++) {
-                        $scope.months[$scope.months.length - 1].weeks[$scope.months[$scope.months.length - 1].weeks.length - 1].days.push({
-                            day: null,
-                            date: null
-                        });
-                    }
-                }
-
-                $scope.months[$scope.months.length - 1].weeks[$scope.months[$scope.months.length - 1].weeks.length - 1].days.push({
-                    day: tempDate.getDay(),
-                    date: tempDate.getDate()
-                });
-
-                if ($scope.months[$scope.months.length - 1].weeks[$scope.months[$scope.months.length - 1].weeks.length - 1].days.length === 7) {
-                    $scope.months[$scope.months.length - 1].weeks.push({
-                        index: 0,
+                populateDaysOnWeeks(tempDate, weekIndex);
+            } else if (monthIsNotPresentOnArray(tempDate)) {
+                weekIndex = 0;
+                $scope.months.push({
+                    monthIndex: tempDate.getMonth(),
+                    weeks: [{
+                        index: weekIndex,
                         days: []
-                    });
-                }
-            }
-            
-            tempDate.setDate(tempDate.getDate() + 1);
-            
-            
-              /*  || monthIsNotPresentOnArray(tempDate)) {
-                months.push(tempDate.getMonth());
+                    }]
+                });
+                populateDaysOnWeeks(tempDate, weekIndex);
             } else {
-                months.push(tempDate.getMonth());
-            }*/
+                populateDaysOnWeeks(tempDate, weekIndex);
+            }
+            tempDate.setDate(tempDate.getDate() + 1);
         }
         console.log($scope.months);
+    }
+
+    function populateDaysOnWeeks(tempDate, weekIndex) {
+        if (tempDate.getDay() > 0 && $scope.months[$scope.months.length - 1].weeks[$scope.months[$scope.months.length - 1].weeks.length - 1].days.length === 0) {
+            for (var z = 0; z < tempDate.getDay() ; z++) {
+                $scope.months[$scope.months.length - 1].weeks[$scope.months[$scope.months.length - 1].weeks.length - 1].days.push({
+                    day: null,
+                    date: null
+                });
+            }
+        }
+
+        $scope.months[$scope.months.length - 1].weeks[$scope.months[$scope.months.length - 1].weeks.length - 1].days.push({
+            day: tempDate.getDay(),
+            date: tempDate.getDate()
+        });
+
+        if ($scope.months[$scope.months.length - 1].weeks[$scope.months[$scope.months.length - 1].weeks.length - 1].days.length === 7) {
+            weekIndex++;
+            $scope.months[$scope.months.length - 1].weeks.push({
+                index: weekIndex,
+                days: []
+            });
+        }
     }
 
     function monthsAreEmpty() {
@@ -66,7 +75,7 @@ app.controller("mainCtrl", function($scope) {
     }
 
     function monthIsNotPresentOnArray(tempDate) {
-        return $scope.months.lastIndexOf(tempDate.getMonth()) === -1;
+        return $scope.months[$scope.months.length - 1].monthIndex !== tempDate.getMonth();
     }
 
 });
