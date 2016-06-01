ebd602cb3fcd25c106575bedeab689b936b74793
Juan Jose Guevara <juanjose@acklenavenue.com>
2016-05-31 19:22:04 -0600

    setting up angular

---
+++ Calendar/src/app.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module("CalendarExercise", ["ngRoute"])
+    .config(function($routeProvider) {
+        $routeProvider.when("/", {
+            templateUrl: "/src/app.html",
+            controller: "mainCtrl"
+        }).otherwise({
+            redirectTo: "/"
+        });
+    });
+app.controller("mainCtrl", function($scope) {
+    $scope.helloWorld = "hello world!";
+});
